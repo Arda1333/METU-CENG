42c3b8b9aea6ccd54e98da111628e404ad579c74
Arda1333 <57459123+Arda1333@users.noreply.github.com>
2022-03-21 22:13:08 +0300

    Added Ceng 140 programming assignments

---
+++ Ceng140/THE2/main.c
@@ -0,0 +1,52 @@
+#include "functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+You will not submit this file, just use for testing your code.
+All your implementations should be in functions.c
+*/
+
+/*
+To compile your code, use "make main"
+Then "./main" to run.
+If you want to compile again, you need to delete the previous executable.
+You may do this by "make clean" or simply "rm main"
+*/
+
+int main(){
+
+    /*
+    Function calls here to test your code
+    Example:
+    float ***sector_times, **laptimes;
+    unsigned int fastest_lap, **positions;
+    sector_times = create_sector_times(...);
+    positions = create_positions(...);
+    lap_times = calculate_lap_times(...);
+    fastest_lap = find_fastest_lap(lap_times, ...);
+    printf("Printing fastest lap to check the answer: %d\n", fastest_lap);
+    */
+
+    unsigned int *n_drivers, *n_laps;
+    int i, j, k;
+    float ***sector_times;
+    *n_drivers = 2;
+    *n_laps = 3;
+
+    sector_times = create_sector_times(n_drivers, n_laps);
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            for(k = 0; k < 3; k++)
+            {
+                printf("%f", sector_times[i][j][k]);
+            }
+            printf("/n");
+        }
+    }
+
+    return 0;
+}
